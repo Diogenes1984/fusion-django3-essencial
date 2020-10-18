504d59d662b85a6e181acb3894d68a19df3c1530
diogenes <diogenes.araujo.palla@gmail.com>
2020-10-18 10:04:08 -0300

    Corrigido Allowed hosts

--- fusion/settings.py
+++ fusion/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['https://fusion-dap.herokuapp.com/']
+ALLOWED_HOSTS = ['fusion-dap.herokuapp.com']
 
 
 # Application definition

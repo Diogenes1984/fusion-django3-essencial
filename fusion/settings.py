f6f0ca71d3d49c1b7673058d7f79984b7519d1cd
diogenes <diogenes.araujo.palla@gmail.com>
2020-10-18 09:58:23 -0300

    Adicionado dotenv

--- fusion/settings.py
+++ fusion/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,13 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o+ud-2#azfy$8wi06)tuv8rw8kcua5pzwl6$ep+e$x7%pgh=xl'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://fusion-dap.herokuapp.com/']
 
 
 # Application definition
